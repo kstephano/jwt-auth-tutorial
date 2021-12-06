4577db2221bf55cbf8fd50861555c9f02071b4af
Kelvin Stephano <kstephano@gmail.com>
2021-12-06 12:20:37 +0000

    created generateAccessToken method

---
+++ helpers/auth.js
@@ -0,0 +1,10 @@
+require('dotenv').config();
+const jwt = require('jsonwebtoken');
+
+function generateAccessToken(user) {
+    return jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, { expiresIn: '15s' });
+}
+
+module.exports = {
+    generateAccessToken
+}
